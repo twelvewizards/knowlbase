fa0bc89ff8861d994e44220996e5bc4a46471a26
twelvewizards <acriss1414@gmail.com>
2024-10-17 22:44:32 +1300

    connected postgresql database to django
    displays the database in plain text at http://127.0.0.1:8000/

--- knowl/views.py
+++ knowl/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from .models import Article
 
 def index(request):
-    return HttpResponse("Welcome to KnowlBase!")
+    # Fetch all articles
+    articles = Article.objects.all()
+
+    # Prepare plain-text response
+    response = "\n".join([f"Title: {article.title}, About: {article.about}, Category ID: {article.category_id}" for article in articles])
+
+    return HttpResponse(response, content_type="text/plain")
